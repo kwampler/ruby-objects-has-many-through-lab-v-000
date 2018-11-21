46e12330c0004306f56dfe30204fd307a36f431e
Kate Preston <katejsenger@yahoo.com>
2018-11-21 17:31:00 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -9,3 +9,14 @@ class Genre
   def songs
     @songs
   end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def artists                     #=> This is how the other two
+    self.songs.collect do |song|  #=> classes talk to each other
+      song.artist
+    end
+  end
+end
